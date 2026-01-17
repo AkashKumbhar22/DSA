36e535a49f1d795022d58a6fb879a8e322821d8f
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-17 22:32:34 +0530

    Solve array questions

--- Session 8/arrays.java
+++ Session 8/arrays.java
@@ -11,6 +11,31 @@
 //     }
 // }
 
+// import java.util.Scanner;
+// public class arrays
+// {
+//     public static void main(String[]args)
+//     {
+//         int []marks = {10,12,223,454,675};
+//         for(int index=0;index<marks.length;index++)
+//             {
+//             System.out.print(marks[index]+" ");
+//         }
+//     }
+// }
+// import java.util.Scanner;
+// public class arrays
+// {
+//     public static void main(String[]args)
+//     {
+//         int []marks = {10,12,223,454,675};
+//         for(int index=0;index<marks.length;index++)
+//             {
+//             System.out.print(marks[index]+" ");
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 public class arrays
 {
