c0e51107a8ea61bec777d8e63ea7cebcccb42fc8
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-20 00:46:50 +0530

    The questions solved

--- Session9/arrays_hw.java
+++ Session9/arrays_hw.java
@@ -43,33 +43,35 @@ public class arrays_hw{
 
 //Find index of an element in array: 
 // Write a Java program to find the index of an array element.
-/* 
-import java.util.Arrays;
-import java.util.Scanner;
-public class arrays_hw{
-    public static void main(String[]args){
-    Scanner sc = new Scanner(System.in);
+// import java.util.Arrays;
+// import java.util.Scanner;
+// public class arrays_hw{
+//     public static void main(String[]args){
+//     Scanner sc = new Scanner(System.in);
 
-System.out.print("Enter the size of array: ");
-int size = sc.nextInt();
+// System.out.print("Enter the size of array: ");
+// int size = sc.nextInt();
 
-int arr1[] = new int[size];
+// int arr1[] = new int[size];
 
-//taking input
-for(int i=0;i<size;i++){
-    arr1[i] = sc.nextInt();
-}
+// //taking input
+// for(int i=0;i<size;i++){
+//     arr1[i] = sc.nextInt();
+// }
 
-System.out.print("Enter the no. to be searched");
-int x = sc.nextInt();
+// System.out.print("Enter the no. to be searched: ");
+// int x = sc.nextInt();
 
-for(int i = 0;i<size;i++){
-    if(x==arr1[i]){
-     System.out.println(i);
-    }
-}
-    }
-} */
+// for(int i = 0;i<size;i++){
+//     if(x==arr1[i]){
+//      System.out.println(i); 
+//      System.out.println("true");
+//     }
+//     }
+//     }
+// } 
+
+
 // Insert element at specific position:
 //  Write a Java program to insert an element (specific position) into an array.
 /*import java.util.Arrays;
@@ -90,33 +92,85 @@ public class arrays_hw{
         
 }
 } */
-/* 
+
+
+// import java.util.Arrays;
+// import java.util.Scanner;
+// public class arrays_hw{
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         int arr[] = {1,2,3,4,6};   
+//     for(int i = 0;i<arr.length;i++){
+//         if(i==0 || i==(arr.length-1)){
+//         }
+//         else{
+//             System.out.print(arr[i]);
+//         }
+
+//     }
+//     }
+// } 
+
+
+
+//Take the array size and array elements as inpt from users and also take teh atrget element which is to be searched in the array
+//if the element is found in the array print true else print false
+
+// import java.util.Arrays;
+// import java.util.Scanner;
+// public class arrays_hw{
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the size of array: ");
+//         int size = sc.nextInt();
+
+//         int arr [] = new int[size];
+//     for(int i=0;i<size;i++){
+//         System.out.print("Enter the element at position "+(i+1)+": ");
+//         arr[i]=sc.nextInt();
+
+//     }
+
+//         System.out.print("Enter the target: ");
+//         int target = sc.nextInt();
+
+//         boolean result = false;
+
+//    for(int element : arr){
+//     if(target == element){
+//         result = true;
+//         break;
+//     }
+//    }
+
+//    if(result){
+//     System.out.println(result);
+//    }
+//    else{
+//     System.out.println(result);
+//    }
+// }
+// }
+
 import java.util.Arrays;
 import java.util.Scanner;
 public class arrays_hw{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int arr[] = {1,2,3,4};   
-    for(int i = 0;i<arr.length;i++){
-        if(i==0 || i==(arr.length-1)){
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+       // nums = sc.nextInt();
+
+        for(int i=0;i<=n;i++){
+            System.out.print("Enter the element " + (i+1)  +" : " );
+           //int nums = sc.nextInt();
+                   nums[i] = sc.nextInt();
         }
-        else{
-            System.out.println(arr[i]);
+        for(int element : nums){
+            System.out.println(element + " ");
         }
-
-    }
-    }
-} */
-
-
-import java.util.Arrays;
-import java.util.Scanner;
-public class arrays_hw{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int arr [] = {1,2,3,4,5};
-        System.out.println("Enter the target: ");
-        int target = sc.nextInt();
-        System.out.println(target);
     }
 }
