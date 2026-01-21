8f587366e143da14ed374bac143225270e72f0cc
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-21 23:56:52 +0530

    hollow square pattern complete

--- Session10/arr_que.java
+++ Session10/arr_que.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 public class arr_que {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Don't miss the task twice: ");
+        //System.out.println("Don't miss the task twice: ");
         int n = 5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
             if(i==1||i==n||j==1||j==n){
-                System.out.print("* ");}
+                System.out.print(" * ");}
             else{
-                System.out.println(" ");   
+                System.out.print("   ");   
             }
             }
+             System.out.println();
         }
-        System.out.println();
+    //    System.out.println();
     }
     
 }
