1a0cd1c8b6531615ecf3b3712b38574540f465d2
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-21 23:49:13 +0530

    square pattern'

--- Session10/arr_que.java
+++ Session10/arr_que.java
@@ -4,9 +4,16 @@ public class arr_que {
         Scanner sc = new Scanner(System.in);
         System.out.println("Don't miss the task twice: ");
         int n = 5;
-        for(int i;i>=n;i++){
-
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+            if(i==1||i==n||j==1||j==n){
+                System.out.print("* ");}
+            else{
+                System.out.println(" ");   
+            }
+            }
         }
+        System.out.println();
     }
     
 }
