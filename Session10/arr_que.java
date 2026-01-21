cf2b9b9acffbde1c361e63bd66dd263ba49f6a0f
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-21 23:42:11 +0530

    error in 'i'

---
+++ Session10/arr_que.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class arr_que {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Don't miss the task twice: ");
+        int n = 5;
+        for(int i;i>=n;i++){
+
+        }
+    }
+    
+}
