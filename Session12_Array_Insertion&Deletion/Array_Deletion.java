2c56a31a5aa1343098d8355ee8d220a82ea2a698
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-28 19:40:54 +0530

    Array_traversal

--- Session12_Array_Insertion&Deletion/Array_Deletion.java
+++ Session12_Array_Insertion&Deletion/Array_Deletion.java
@@ -1,20 +1,49 @@
 import java.util.Arrays;
 import java.util.Scanner;
 class Array_Deletion{
+        //Function to print the array
+        public static void Display(int n,int actual_values[]){
+            for(int i=0;i<n;i++){
+            System.out.println("The element at position:" + i +"is:" + " " + actual_values[i] );
+            }
+        } 
+
+        //Function for delete
+        // public static void delete{
+        //     arr[n-1]
+        // };
+
     public static void main(String args[]){
         int arr[] = new int[100];
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of n(no. of actual elements): ");
         int n = sc.nextInt();
-            int actual_values [] = new int[n];
+        int actual_values [] = new int[n];
 
        // int actual_value [] = new int[n];
         for(int i=0;i<n;i++){
-            System.out.print("Enter the elments at position: " + i );
+            System.out.print("Enter the elments at position :" + i +": " );
             actual_values[i] = sc.nextInt();
             }
 
-            System.out.println(Arrays.toString(actual_values));
+     Display(n,actual_values);
+
+
+
+     //Deleting last element in array
+     //We have used the if conition what if the array is lement we cannot delete anything
+     if(n>=1){
+    //  for(int i = 0;i<n-1;i++){
+    //     System.out.println(actual_values[i]);
+    //  }
+     //OR
+     System.out.println("Deleting the last element in array: ");
+     actual_values[n-1]=0;
+     n--;
+
+    Display(n,actual_values);
+     
     }
+}
 }
---
+++ Session12_Array_Insertion&Deletion/Array_Traversal.java
@@ -0,0 +1,10 @@
+public class Array_Traversal {
+    public static void main(String[] args){
+    int arr[] = {1,2,3,4,5};
+
+    //It is technique to traversal in array 
+    for(int no : arr){
+        System.out.println(no);
+    }
+    }
+}
