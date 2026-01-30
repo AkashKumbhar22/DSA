c522410c02af531aa020e80347c98bff7ac4a246
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-01-30 09:39:35 +0530

    practice questions

---
+++ Session13/mult_array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
+class mult_array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter the size of array: ");
+    //   int n = sc.nextInt();
+
+    //   int sub_array = (n*(n+1))/2;
+    //   System.out.println("The size of sub_array is: "+sub_array);
+
+      int arr[] = {5,2,1,3,1};
+
+    //   int start =0;
+      
+      for(int start=0;start<arr.length;start++){
+            System.out.println(arr[start]);
+            for(int j=start;j<arr.length;j++){
+                if(j>=start){
+                System.out.print( arr[start] +","+arr[j]);
+                }
+            }
+            System.out.println();
+                
+      }
+}
+}
