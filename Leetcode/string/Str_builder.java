e2500d5a892b665cd48cb179c86b5619aa0a86ad
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-06 20:22:27 +0530

    TwoSum

--- Leetcode/string/Str_builder.java
+++ Leetcode/string/Str_builder.java
@@ -14,22 +14,41 @@
 
 
 
-//Write a function that reverse a string using char[]
-public class Str_builder {
-    public static void main(String[] args) {
-         int arr[] = {1,2,3,4,5};
-         int target = 5;
+// //Write a function that reverse a string using char[]
+// public class Str_builder {
+//     public static void main(String[] args) {
+//          int arr[] = {1,2,3,4,5};
+//          int target = 5;
 
-         for(int i=0;i<arr.length;i++){
-               for(int j=i+1;j<arr.length;j++){
-                    if((arr[i]+arr[j])==target){
-                        System.out.println(i+","+j);
+//          for(int i=0;i<arr.length;i++){
+//                for(int j=i+1;j<arr.length;j++){
+//                     if((arr[i]+arr[j])==target){
+//                         System.out.println(i+","+j);
                         
-                    }
+//                     }
                     
-             //   }
-         }           
-         }    
-}}
+//              //   }
+//          }           
+//          }    
+// }}
+
+public class Str_builder{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5};
+        int target = 4 ;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1 ; j<arr.length ; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(i + ","+j);
+                    System.out.println("the value of indices are" +arr[i]+","+arr[j]);
+                    //System.out.println(arr[j]);
+                }
+            }
+           break;
+        }
+    }
+
+}
 
 
