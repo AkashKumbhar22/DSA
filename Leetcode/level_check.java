15fc73c9cfa19cf3f928237861c88fcf29be87a3
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-15 21:35:31 +0530

    two pointer

---
+++ Leetcode/level_check.java
@@ -0,0 +1,21 @@
+// Declare an integer array with values {1, 2, 3, 4, 5} 
+// and print all elements using a for-loop.
+
+
+// public class level_check {
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,4,5};
+//         for(int i=0;i<arr.length;i++)
+//             System.out.println(arr[i]);
+//     }
+// }
+
+
+
+// Write a Java function called findMax that:
+// Takes an integer array as input
+// Returns the maximum value in the array
+// Handle empty array (return Integer.MIN_VALUE)
+// Write the complete function 
+
+    class findMax(int arr[])
