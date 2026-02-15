25cffc27dbe55697e643219b73996596546bb414
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-15 21:35:55 +0530

    two pointer

--- Leetcode/Two_Pointer/ponter.java
+++ Leetcode/Two_Pointer/ponter.java
@@ -1,39 +1,3 @@
-// package Two_Pointer;
-// import java.util.Scanner;
-
-
-// public class two_pointer {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-
-//         int arr[] = {1,2,3,4,5};
-
-//         int target = 10;
-//         int left = 0;
-//         int right = arr.length-1;
-//         boolean found = false;
-
-//         while(left<right){
-//             if(arr[left]+arr[right]==target){
-//                 System.out.println("The index of sum of target is : "+ left +","+right);
-//                 found = true;
-//                 break;
-//             }
-//             else if(arr[left]+arr[right]>target){
-//                 right--;
-//             }
-//              else if(arr[left]+arr[right]<target){
-//                 left++;
-//             }
-           
-//         }
-//          if(!found){
-//                 System.out.println("Target not found");
-//             }
-//     }
-// }
-
-
 package Two_Pointer;
 import java.util.Scanner;
 
@@ -68,4 +32,3 @@ public class two_pointer {
             }
     }
 }
-
