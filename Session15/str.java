49b4a7a16903add1d8ff2ce618973ec9a1df183a
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-16 15:22:19 +0530

    Strings

---
+++ Session15/str.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class str {
+    public static void main(String[] args) {
+        String st = "Akash Shiva Kumbhar";
+        //System.out.print(st.charAt(2));    //It gives the character at the specific at the specific length
+      //  System.out.println(st.substring(0,5));      //It gives the charcter between starting index and Ending index
+
+
+      //In these technique of ("array.split("The value at which it must get splited")") whenever teh value appers the split is formed as can been seen
+
+    //   String arr[] = st.split("a");
+
+    
+    //   System.out.println(Arrays.toString(arr));
+
+    //The "array_name.contains()"
+    //System.out.println(st.contains("Kumbhar"));
+    
+    String ak = "Akash Shiva Kumbhar";
+    System.out.println(ak.trim());
+    }
+}
