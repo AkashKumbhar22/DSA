2a1a5164b5a59fb6c07b3fad94865a3c5daf62a0
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-03-21 23:06:05 +0530

    code prac

--- Udemy/Reverse_array.java
+++ Udemy/Reverse_array.java
@@ -17,21 +17,3 @@ public class Reverse_array{
     }
 }
 
-public class Reverse_array{
-    public static void main(String[] args){
-        int arr[] = {2,4,6,8,10};
-        int start = 0;
-        int end = arr.length - 1;
-
-     while(start <= end){
-     int temp = arr[start];
-     arr[start] = arr[end];
-     arr[end] = temp;
-
-        System.out.println("The element at index" + start + ": " + (arr[start]) );
-        start ++;
-        end--;
-     }
-
-    }
-}
