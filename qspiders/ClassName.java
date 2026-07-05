d8a51bf21a198baf9ba6d440366061c6b9f55bc6
AkashKumbhar22 <akashkumbhar2912@gmail.com>
2026-07-05 11:24:35 +0530

    classname as non-primitive datatype

---
+++ qspiders/ClassName.java
@@ -0,0 +1,10 @@
+//Using classname as non primitive datatype
+class ClassName
+{
+	public static void main(String[] args)
+	{
+	ClassName a;
+	a = 0;
+	System.out.println(a);
+	}
+	}
---
+++ qspiders/j2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+
+
+public static int add(int n, int m){
+	return n + m;
+}
+
+
+
+
+public static void main (String[] args)
+{	
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the value of n");
+int a = sc.nextInt();
+
+		System.out.println(add(69,96));
+}
