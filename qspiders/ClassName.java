ff0a433a83032c597c3f2ed95e4f5e6158f7cbc7
AkashKumbhar22 <akashkumbhar2912@gmail.com>
2026-07-05 14:35:09 +0530

    classname as non-primitive datatype

--- qspiders/ClassName.java
+++ qspiders/ClassName.java
@@ -1,10 +1,21 @@
 //Using classname as non primitive datatype
+/*
 class ClassName
 {
 	public static void main(String[] args)
 	{
 	ClassName a;
-	a = 0;
+	a = null;
 	System.out.println(a);
 	}
+	}
+	*/
+	class ClassName{
+		int a = 16;
+		public static void main(String[] args)
+		{
+			int a = 10;
+			int az = 20;
+			
+		}System.out.println(a);
 	}
