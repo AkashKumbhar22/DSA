b750576a708922559f08cb1b2f45e0e0a00c2f00
AkashKumbhar22 <akashkumbhar2912@gmail.com>
2026-07-26 22:23:30 +0530

    dsa practice

---
+++ qspiders/loops/Sort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays.Scanner;
+class Sort{
+	public static void main(String[] args){
+	
+	Scanner sc = new Scanner(System.in)
+	
+	System.out.print(
+	int c = sc.nextInt();
+	int arr[] = {23, 432, 3 , -1 , -2};
+	
+	Arrays.sort(arr);
+	int count = 0;
+	int max = arr[0];
+for(int i = 0 ; i < arr.length ; i++)
+{
+	if(arr[i] < 0)
+		count++;
+	System.out.print(arr[i] + " ");
+}
+			
+
+	}
+	}
